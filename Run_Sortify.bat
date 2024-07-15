8fc22b94508436df5a896cf3119e99c029b2a008
IDGBAN <106408231+IDGBAN@users.noreply.github.com>
2024-07-14 20:51:22 -0400

    cleaner output text

--- Run_Sortify.bat
+++ Run_Sortify.bat
@@ -3,8 +3,13 @@ echo Downloading script...
 powershell -Command "(New-Object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/IDGBAN/Sortify/main/Sortify_Script.py', 'Sortify_Script.py')"
 echo Script downloaded!
 echo Running script...
+echo:
 python Sortify_Script.py
+echo:
 echo Deleting script...
 del Sortify_Script.py
-echo Process Successful!
+echo:
+echo Analysis Successful!
+echo:
+echo:
 timeout /t 10
