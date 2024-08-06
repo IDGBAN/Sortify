b3c44a17cc227ac3bd83b3f31a18055bdc521e5d
IDGBAN <106408231+IDGBAN@users.noreply.github.com>
2024-08-06 03:31:58 -0400

    Update Sortify_Script.py

--- Sortify_Script.py
+++ Sortify_Script.py
@@ -45,61 +45,39 @@ def analyze_listening_data(json_file_paths, aggregate_by='artist'):
             print(f"Warning: File not found: {json_file_path}")
             continue  # Skip to the next file if not found
 
+    return playtimes, counts, total_tracks, total_ms_played
+
+
 def format_timedelta(td):
-
     """Formats a timedelta object into HH:MM:SS."""
-
     total_hours = td.days * 24 + td.seconds // 3600
-
     total_minutes = (td.seconds % 3600) // 60
-
     total_seconds = td.seconds % 60
-
     return f"{total_hours:02d}:{total_minutes:02d}:{total_seconds:02d}"
 
 
-
 def write_results(output_file, results_type, playtimes, counts, total_tracks, total_ms_played):
-
     """Writes the analysis results to a file."""
 
-
-
     total_time_played = timedelta(milliseconds=total_ms_played)
-
     ranked_by_time = sorted(playtimes.items(), key=itemgetter(1), reverse=True)
-
     ranked_by_count = counts.most_common()
-
-
 
     os.makedirs(os.path.dirname(os.path.abspath(output_file)), exist_ok=True)
 
-
-
     with open(output_file, "w", encoding="utf-8") as outfile:
-
         outfile.write(f"Total Play Count: {total_tracks}\n")
-
         outfile.write(f"Total Listening Time: {format_timedelta(total_time_played)}\n\n\n")
 
-
-
         outfile.write(f"{results_type} Ranked by Play Count:\n")
-
         for rank, (item, count) in enumerate(ranked_by_count, 1):
-
             outfile.write(f"{rank}. {count} times - {item}\n")
 
-
-
         outfile.write(f"\n{results_type} Ranked by Listening Time:\n")
-
         for rank, (item, ms_played) in enumerate(ranked_by_time, 1):
-
             playtime = timedelta(milliseconds=ms_played)
-
             outfile.write(f"{rank}. {format_timedelta(playtime)} - {item}\n")
+
 
 if __name__ == "__main__":
     json_file_patterns = glob.glob("*.json")
@@ -119,3 +97,4 @@ if __name__ == "__main__":
     write_results(output_file_tracks, "Tracks", track_playtimes, track_counts, total_tracks, total_ms_played)
 
     print(f"Results saved to {output_file_tracks} and {output_file_artists}")
+
